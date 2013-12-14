35f05dfc4e9900c01b6f876cd8019b72d6b25dd2
Eric Brown <admin@ironalloygames.com>
2013-12-14 14:00:56 -0800

    N

--- ringofoil/src/com/ironalloygames/ringofoil/entity/PistonEntity.java
+++ ringofoil/src/com/ironalloygames/ringofoil/entity/PistonEntity.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.joints.PrismaticJoint;
 import com.badlogic.gdx.physics.box2d.joints.PrismaticJointDef;
 import com.ironalloygames.ringofoil.ArenaState;
 import com.ironalloygames.ringofoil.RG;
@@ -12,6 +13,8 @@ import com.ironalloygames.ringofoil.component.Component;
 import com.ironalloygames.ringofoil.component.Piston;
 
 public class PistonEntity extends ComponentEntity {
+
+	PrismaticJoint joint;
 
 	Piston piston;
 
@@ -28,14 +31,14 @@ public class PistonEntity extends ComponentEntity {
 	public void commandKeyDown() {
 		super.commandKeyDown();
 
-		System.out.println("EXTEND!");
+		joint.setMotorSpeed(100);
 	}
 
 	@Override
 	public void commandKeyUp() {
 		super.commandKeyUp();
 
-		System.out.println("RETRACT!");
+		joint.setMotorSpeed(-100);
 	}
 
 	@Override
@@ -53,8 +56,14 @@ public class PistonEntity extends ComponentEntity {
 
 		PrismaticJointDef jd = new PrismaticJointDef();
 		jd.initialize(body, rodBody, body.getPosition(), new Vector2(1, 0));
+		jd.enableMotor = true;
+		jd.enableLimit = true;
+		jd.lowerTranslation = 0;
+		jd.upperTranslation = component.getBoundingBox().x * .9f;
+		jd.maxMotorForce = 10;
 
-		((ArenaState) RG.currentState).world.createJoint(jd);
+		joint = (PrismaticJoint) ((ArenaState) RG.currentState).world
+				.createJoint(jd);
 	}
 
 	@Override
