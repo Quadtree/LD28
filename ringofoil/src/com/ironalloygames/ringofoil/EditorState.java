06a7b32cb563f67fe17411178450ecbcd780849e
Eric Brown <admin@ironalloygames.com>
2013-12-14 23:04:24 -0800

    N

--- ringofoil/src/com/ironalloygames/ringofoil/EditorState.java
+++ ringofoil/src/com/ironalloygames/ringofoil/EditorState.java
@@ -79,7 +79,7 @@ public class EditorState extends GameState {
 		if (paletteItemSelected >= 0) {
 			RG.am.getFont().drawWrapped(RG.batch, "Selected: " + palette.get(paletteItemSelected).desc + "\nClick on a green spot to place this item, or right click to deselect.", -400, 420, 800);
 		} else {
-			RG.am.getFont().drawWrapped(RG.batch, "Left click an item in the palette to select it.", -400, 420, 800);
+			RG.am.getFont().drawWrapped(RG.batch, "Left click an item in the palette to select it. Click a part of the robot to select it.", -400, 420, 800);
 		}
 
 		RG.am.getFont().drawWrapped(RG.batch, "Commands: Esc - Return to Main Menu, S - Save, L - Load", -400, -420, 800);
@@ -129,8 +129,6 @@ public class EditorState extends GameState {
 
 						AABB nBox = new AABB(nmax, nmin);
 
-						// System.out.println(nur + " " + nll);
-
 						boolean intersection = false;
 
 						for (Component tc : robot.getComponents()) {
@@ -142,19 +140,7 @@ public class EditorState extends GameState {
 
 							AABB box = new AABB(max, min);
 
-							// System.out.println(tc + ": " + ll + " " + ur);
-
-							/*
-							 * for (Vector2 pt : pts) {
-							 * 
-							 * if (pt.x > ll.x && pt.y > ll.y && pt.x < ur.x &&
-							 * pt.y < ur.y) intersection = true; }
-							 */
-
 							if (nBox.intersects(box)) {
-								// System.out.println("INTERSECTION " + nur +
-								// " " +
-								// nll + " " + ur + " " + ll);
 								intersection = true;
 							}
 						}
@@ -166,6 +152,8 @@ public class EditorState extends GameState {
 					}
 				}
 			}
+		} else if (button == Buttons.RIGHT) {
+			paletteItemSelected = -1;
 		}
 
 		return super.touchDown(screenX, screenY, pointer, button);
